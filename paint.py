4857ef42a77eab682a42b25cc98ef96a5de7b439
parzivalhaliday <55963388+parzivalhaliday@users.noreply.github.com>
2023-02-08 01:45:19 +0300

    Update paint.py

--- paint.py
+++ paint.py
@@ -1,3 +1,5 @@
+# Code to color a image in the Paint application using PyAutoGUI
+
 import pyautogui as pag
 from PIL import Image
 import time
@@ -5,29 +7,27 @@ import requests
 import io
 import pandas
 
+# Fetch the image from the URL
 response = requests.get('image link')
 image_byte = io.BytesIO(response.content)
 foto = Image.open(image_byte)
 
+# Convert the image to grayscale
 foto = foto.convert('L')
 
+# Resize the image to reduce the processing time
 oran = 0.5
 foto = foto.resize((int(foto.size[0]*oran),int(foto.size[1]*oran)))
 x,y = foto.size
 
-print(x,y)
-input('hazır olunca entere bas\n')
-
-xsayac = 0
-ysayac = 0
-
-pag.PAUSE = 0.0005
-
-koordinat = []
-renk = []
-
+# Get the data from the image
 fotodata = foto.getdata()
 
+# Store the coordinate and color data in two separate lists
+xsayac = 0
+ysayac = 0
+koordinat = []
+renk = []
 for data in fotodata:
     renk.append(data)
     koordinat.append((xsayac, ysayac))
@@ -37,25 +37,29 @@ for data in fotodata:
         xsayac = 0
         ysayac += 1
 
+# Store the coordinate and color data in a dataframe
 bilgiler = {'koordinat':koordinat, 'renk':renk}
 dataframe = pandas.DataFrame(bilgiler).sort_values('renk')
 
+# Set the initial color value to 0
 renkkodu = 0
+
+# Loop through the sorted dataframe and color each pixel
 sayac = 0
 for i in zip(dataframe['koordinat'], dataframe['renk']):
     sayac += 1
     start = time.time()
-    print(i[0], i[1])
-    print(sayac / (x*y) )
-    print('-'*35)
     
+    # Check if the color value has changed
     if renkkodu != i[1]:
         renkkodu = i[1]
-        # Renk Seçimine tıkla
+        # Click the color select button
         pag.click(1060, 75)
 
+        # Wait for the color select dialog to appear
         time.sleep(0.05)
-        # Renk ayarlarını gir
+        
+        # Enter the color values in the dialog
         pag.click(1165, 595)
         for _ in range(3):
             pag.press('backspace')
@@ -74,8 +78,9 @@ for i in zip(dataframe['koordinat'], dataframe['renk']):
             time.sleep(0.05)
         pag.typewrite(str(i[1]))
 
-        # Tamam'a Tıkla
+        # Click the 'OK' button
         pag.click(777, 665)
         time.sleep(0.05)
     
+    # Click the current pixel
     pag.click(8 + i[0][0], 147 + i[0][1])
